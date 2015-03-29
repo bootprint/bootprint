e192adf77e2e11646a990dc89351a0172b07546f
Nils Knappmeier <n.knappmeier@i-views.de>
2015-03-29 23:48:39 +0200

    Fix testcases

--- tests/builder_test.js
+++ tests/builder_test.js
@@ -1,37 +1,43 @@
 var _ = require("lodash");
 var assert = require("assert");
 
-module.exports.testBuilder = function(test) {
+module.exports.testBuilder = function (test) {
     test.expect(4);
     var initialValue = {"c": 3};
-    var builder = require("../src/builder.js")(initialValue);
+    var BootprintBuilder = require("../src/BootprintBuilder.js");
+    var builder = new BootprintBuilder(initialValue);
 
-    builder = builder.override({a: 1});
+    builder = builder.merge({a: 1});
     test.deepEqual(_.omit(builder._options, "override"), {a: 1, c: 3});
 
-    builder = builder.override({a: 2, b: [1, 2, 3]});
+    builder = builder.merge({a: 2, b: [1, 2, 3]});
     test.deepEqual(_.omit(builder._options, "override"), {c: 3, a: 2, b: [1, 2, 3]});
 
-    builder = builder.override({b: [4, 5, 6]});
+    builder = builder.merge({b: [4, 5, 6]});
     test.deepEqual(_.omit(builder._options, "override"), {c: 3, a: 2, b: [1, 2, 3, 4, 5, 6]});
 
     // Initial config value must not have changed
-    test.deepEqual(initialValue, {c:3});
+    test.deepEqual(initialValue, {c: 3});
     test.done();
 };
 
-module.exports.testExtendPartialsDef = function(test) {
-    var builder = require("../src/builder.js")({
-        partials: { name : 'aDirectory'}
+module.exports.testExtendPartialsDef = function (test) {
+
+    var BootprintBuilder = require("../src/BootprintBuilder.js");
+    var builder = new BootprintBuilder();
+
+    builder = builder.merge({
+        partials: 'aDirectory'
     });
     test.deepEqual(builder._options, {
-        partials: { name : [ 'aDirectory' ]}
+        // Partials are expanded to arrays
+        partials: ['aDirectory']
     });
-    builder = builder.override({
-        partials: { name : 'secondDirectory' }
+    builder = builder.merge({
+        partials: 'secondDirectory'
     });
     test.deepEqual(builder._options, {
-        partials: { name : [ 'aDirectory','secondDirectory' ]}
+        partials: ['aDirectory', 'secondDirectory']
     });
     test.done();
 };
--- tests/partials_test.js
+++ tests/partials_test.js
@@ -1,4 +1,4 @@
-var loadPartials = require("../src/load-partials.js");
+var loadPartials = require("../src/readPartials.js");
 var path = require("path");
 var deep = require("q-deep");
 
@@ -7,17 +7,16 @@ var prefix = "abc";
 
 module.exports.testPartials = function (test) {
     test.expect(1);
-    deep(loadPartials({
-        "num": [path.join(__dirname, "testPartials1"), path.join(__dirname, "testPartials2")],
-        "letter": [path.join(__dirname, "testPartialsA")],
-        "directFile": [path.join(__dirname, "testPartialsA","vier.hbs")]
-    })).done(function (result) {
+    deep(loadPartials([
+            path.join(__dirname, "testPartials1"),
+            path.join(__dirname, "testPartials2"),
+            path.join(__dirname, "testPartialsA")
+    ])).done(function (result) {
         test.deepEqual(result, {
-                'num/eins': 'Eins',
-                'num/zwei': 'Two',
-                'num/drei': 'Three',
-                'letter/vier': 'Vier',
-                "directFile" : 'Vier'
+                'eins': 'Eins',
+                'zwei': 'Two',
+                'drei': 'Three',
+                'vier': 'Vier',
             }
         );
         test.done();
