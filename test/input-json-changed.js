81973f4874f1977eab309be633e9b9fbee4caaa9
Nils Knappmeier <n.knappmeier@i-views.de>
2015-11-10 21:06:32 +0100

    Testcase for issue #8

---
+++ test/input-json-changed.js
@@ -0,0 +1,64 @@
+/*!
+ * bootprint <https://github.com/nknapp/bootprint>
+ *
+ * Copyright (c) 2015 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+var path = require('path')
+var fs = require('fs')
+require('trace')
+require('clarify')
+process.on('exit', function () {
+  var unhandledReasons = require('q').getUnhandledReasons();
+  if (unhandledReasons.length>0) {
+    console.log(unhandledReasons)
+  }
+})
+
+/* global describe */
+/* global it */
+// /* global before */
+var expect = require('chai').expect
+var bootprint = require('../')
+var tmpDir = path.join(__dirname, 'tmp')
+var targetDir = path.join(tmpDir, 'target')
+var swaggerJsonFile = path.join(tmpDir, 'changing.json')
+try {
+  fs.mkdirSync(tmpDir)
+} catch (e) {
+  // ignored
+}
+
+function run () {
+  return bootprint
+    .merge({
+      handlebars: {
+        templates: path.join(__dirname, 'fixtures', 'handlebars')
+      },
+      less: {
+        main: require.resolve('./fixtures/main.less')
+      }
+    })
+    .build(swaggerJsonFile, targetDir)
+    .generate()
+}
+
+describe('The input json', function () {
+  it('should be loaded on each bootprint-run, even within the same process', function () {
+    fs.writeFileSync(swaggerJsonFile, JSON.stringify({
+      eins: 'one', zwei: 'two', drei: 'three'
+    }))
+    return run().then(function () {
+      var content = fs.readFileSync(path.join(targetDir, 'index.html'), {encoding: 'utf-8'})
+      expect(content.trim()).to.equal('eins: one\nzwei: two\ndrei: three')
+      fs.writeFileSync(swaggerJsonFile, JSON.stringify({
+        eins: 'un', zwei: 'deux', drei: 'trois'
+      }))
+      return run()
+    }).then(function () {
+      var content = fs.readFileSync(path.join(targetDir, 'index.html'), {encoding: 'utf-8'})
+      expect(content.trim()).to.equal('eins: un\nzwei: deux\ndrei: trois')
+    })
+  })
+})
